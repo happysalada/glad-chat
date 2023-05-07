4dcdca460c6178640c0d881495b54af4612553ec
happysalada <raphael@megzari.com>
2023-05-07 12:34:44 -0400

    try more results from qdrant

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -68,7 +68,7 @@ export const actions = {
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({
 						vector: embedding,
-						limit: 10,
+						limit: 30,
 						with_payload: true,
 					}),
 				}
